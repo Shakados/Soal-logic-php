91fe262181d2e242db27d52a80ef2a77cb11f9d1
Shakados <83188149+Shakados@users.noreply.github.com>
2021-05-03 20:08:10 +0700

    Create agil_soal_5.php

---
+++ agil_soal_5.php
@@ -0,0 +1,9 @@
+<?php
+$menu = ["Makanan", "Minuman", "Parsel"];
+foreach($menu as $v){
+    echo $v." <br>";
+    echo "1. Menu $v ke 1 <br>";
+    echo "2. Menu $v ke 2 <br>";
+    echo "3. Menu $v ke 3 <br>";
+}
+?>
