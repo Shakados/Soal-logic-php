eb4d596381960a98367fcc52b34c086f76beeee0
Shakados <83188149+Shakados@users.noreply.github.com>
2021-05-03 19:59:17 +0700

    Create agil_soal_2.php

---
+++ agil_soal_2.php
@@ -0,0 +1,10 @@
+<?php
+const HARGA_MAINAN = 55000;
+$cash = 3*10000 + 4*5000 + 5*20000;
+$uangSetyo = $cash-HARGA_MAINAN;
+echo "Harga Mainan = 55000 <br>";
+echo "Uang Setyo = (3*10000) + (4*5000) + (5*20000) <br>";
+echo "Uang Setyo = $cash <br>";
+echo "Sisa Uang Setyo = Uang Setyo - Harga Mainan <br>";
+echo "Sisa Uang Setyo = $uangSetyo <br>";
+?>
